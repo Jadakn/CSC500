ca55ac56a135f5231352a443bb1f9fd15ca1939d
Jadakn <JaredKi@users.noreply.github.com>
2025-10-07 09:20:29 -0500

    Added handling to ensure numeric inputs

--- Module 1/two_numbers.py
+++ Module 1/two_numbers.py
@@ -1,9 +1,18 @@
-num1 = input("Enter the first number: ")
-num2 = input("Enter the second number: ")
-print("The sum is:", float(num1) + float(num2))
-print("The difference is:", float(num1) - float(num2))
-print("The product is:", float(num1) * float(num2))
 try:
-    print("The quotient is:", float(num1) / float(num2))
+    num1 = float(input("Enter the first number: "))
+except ValueError:
+    print("Error: Please enter a numeric value for the first number.")
+    exit()
+try:
+    num2 = float(input("Enter the second number: "))
+except ValueError:
+    print("Error: Please enter a numeric value for the second number.")
+    exit()
+    
+print("The sum is:", num1 + num2)
+print("The difference is:", num1 - num2)
+print("The product is:", num1 * num2)
+try:
+    print("The quotient is:", num1 / num2)
 except ZeroDivisionError:
     print("Error: Cannot divide by zero.")
