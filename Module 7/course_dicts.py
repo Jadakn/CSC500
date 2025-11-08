d2938dc7404f4f690115dc396557430b470d7a15
Jadakn <JaredKi@users.noreply.github.com>
2025-11-08 13:18:17 -0600

    Module 7 dictionary and input

---
+++ Module 7/course_dicts.py
@@ -0,0 +1,43 @@
+course_to_room_number_dict = {
+    "CSC101": "3004",
+    "CSC102": "4501",
+    "CSC103": "6755",
+    "NET110": "1244",
+    "COM241": "1411"
+}
+
+course_to_instructors_dict = {
+    "CSC101": "Haynes",
+    "CSC102": "Alvarado",
+    "CSC103": "Rich",
+    "NET110": "Burke",
+    "COM241": "Lee"
+}
+
+course_to_times_dict = {
+    "CSC101": "8:00 a.m.",
+    "CSC102": "9:00 a.m.",
+    "CSC103": "10:00 a.m.",
+    "NET110": "11:00 a.m.",
+    "COM241": "1:00 p.m."
+}
+
+while True:
+    while True:
+        try:
+            course = input("Enter a course number (ex: CSC101): ")
+            if course not in course_to_room_number_dict:
+                print("Course not found. Please enter a valid course number.")
+                continue
+            break
+        except ValueError:
+            print("Error: Input should be a valid course number.")
+            continue
+
+    if course in course_to_room_number_dict:
+        print(f"Course Number: {course}")
+        print(f"    Room Number: {course_to_room_number_dict[course]}")
+        print(f"    Instructor: {course_to_instructors_dict[course]}")
+        print(f"    Meeting Time: {course_to_times_dict[course]}")
+    else:
+        print("Course not found.")
